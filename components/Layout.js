fe76504eaff70efc4974cca8a5f771e21a208e54
Parv-gugnani <9765.parv@gmail.com>
2023-09-22 13:54:59 +0530

    layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,10 @@
 import { Sora } from "@next/font/google";
 
-
-const sora
+const sora = Sora({
+  subsets: ["latin"],
+  variable: "--font-sora",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+});
 
 const Layout = () => {
   return <div>Layout</div>;
